eb2b35499334ffcde1707a01efcce023cc355bfa
Uygar Deniz ACAR <uygar.d.acar@gmail.com>
2024-12-18 07:53:27 +0300

    Update User model to return full name

--- backend/users/models.py
+++ backend/users/models.py
@@ -12,7 +12,11 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
     def __str__(self):
-        return self.username
+        return self.full_name
     
 
